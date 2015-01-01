ac483209ae9dbb32c8609b3cf6e69aaa85c27e94
Marcin Korniluk <marcin.korniluk@polidea.com>
2015-01-01 15:07:39 +0100

    svg samples, updated buttons, library 0.1.4

--- app/src/main/java/pl/zielony/materialsamples/SVGActivity.java
+++ app/src/main/java/pl/zielony/materialsamples/SVGActivity.java
@@ -3,18 +3,13 @@ package pl.zielony.materialsamples;
 import android.app.Activity;
 import android.os.Bundle;
 
-import pl.zielony.material.widget.Button;
-
 /**
  * Created by Marcin on 2014-12-15.
  */
-public class ButtonsActivity extends Activity {
+public class SVGActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buttons);
-
-        Button button = (Button) findViewById(R.id.bunny);
-        button.setRect(false);
+        setContentView(R.layout.activity_svg);
     }
 }
