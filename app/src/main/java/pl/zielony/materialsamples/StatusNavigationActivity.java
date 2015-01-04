09cd1bf31cbdbfd354913f059297a5ae2e4dd01f
Marcin Korniluk <marcin.korniluk@polidea.com>
2015-01-04 23:35:11 +0100

    added StatusBar and NavigationBar, xml attributes rewritten, added new sample

---
+++ app/src/main/java/pl/zielony/materialsamples/StatusNavigationActivity.java
@@ -0,0 +1,17 @@
+package pl.zielony.materialsamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pl.zielony.material.widget.Button;
+
+/**
+ * Created by Marcin on 2014-12-15.
+ */
+public class StatusNavigationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_navigation);
+    }
+}
